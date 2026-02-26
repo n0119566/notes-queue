d13c151ca52d1da48fffd29c80533b29c1c98be7
Chris <127966206+n0119566@users.noreply.github.com>
2026-02-26 06:28:31 -0800

    Adding code to delete pinecone records as well

--- src/jobs/cleanupDeletedNotes.ts
+++ src/jobs/cleanupDeletedNotes.ts
@@ -1,6 +1,7 @@
 import { Job } from "agenda";
 import Note from "../models/Note";
 import { MAX_DELETION_DAYS } from "../variables";
+import { Pinecone } from "@pinecone-database/pinecone/dist/pinecone";
 
 /**
  * Job: cleanup-deleted-notes
@@ -17,6 +18,45 @@ export const cleanupDeletedNotesJob = async (job: Job): Promise<void> => {
     // Calculate the date 30 days ago
     const thirtyDaysAgo = new Date();
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - MAX_DELETION_DAYS);
+
+    // Find notes that are marked as deleted and older than 30 days
+    const vectorsToDelete = await Note.find({
+      deleted: true,
+      $or: [
+        { deletedDate: { $lt: thirtyDaysAgo } }, // If date is greater than 30 days ago
+        { deletedDate: { $exists: false } }, // OR if the field is missing entirely
+      ],
+    }).select("_id");
+
+    if (vectorsToDelete.length === 0) {
+      console.log(`✅ [${jobName}] No old deleted notes found to clean up.`);
+      return;
+    }
+
+    const pc = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY || "",
+    });
+
+    const ids = vectorsToDelete.map((note) => note._id.toString());
+
+    const indexName = process.env.PINECONE_INDEX_NAME || "notes";
+    const index = pc.index({ name: indexName });
+
+    if (ids.length <= 1000) {
+      await index.deleteMany(ids);
+    } else {
+      const result: string[][] = [];
+      for (let i = 0; i < ids.length; i += 1000) {
+        const chunk = ids.slice(i, i + 1000);
+        result.push(chunk);
+      }
+
+      await Promise.all(
+        result.map(async (chunk) => {
+          await index.deleteMany(chunk);
+        }),
+      );
+    }
 
     // Find and delete notes that are marked as deleted and older than 30 days
     const result = await Note.deleteMany({
